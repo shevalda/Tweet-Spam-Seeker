38c4ce04214615d3ed111bbbbab911809ba75c67
VisionXV <jonathantjandra.98@outlook.com>
2018-04-22 21:51:38 +0700

    edit main - regex

--- tools/main.py
+++ tools/main.py
@@ -20,13 +20,16 @@ if __name__ == "__main__" :
     howto = json.load(open('tools/json/user_input.json'))
     tarr = []
     mode = howto['algorithm']
-    patt = howto['keyword']
+    if (mode != "regex") :
+        patt = howto['keyword'].lower() 
+    else :
+        patt = howto['keyword']
     for a in range(len(data)) :
-        tweet = data[a]['text']
+        tweet = data[a]['text'].lower()
         isSpam = chooser(mode,patt,tweet)
         ResObject = dict()
         ResObject['spam'] = isSpam
-        ResObject['text'] = tweet
+        ResObject['text'] = data[a]['text']
         ResObject['username'] = data[a]['user']['screen_name']
         ResObject['name'] = data[a]['user']['name']
         tarr.append(ResObject)
