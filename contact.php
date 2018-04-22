b5a1382aab9d5114dc76879141a00f816f0f5ef6
Shevalda Gracielira <nightlyfurious@gmail.com>
2018-04-23 02:55:38 +0700

    web design completed

---
+++ contact.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset="UTF-8">
+
+    <meta http-equiv="X-UA-Compartible" content="IE=Edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Spam Seeker - Contact</title>
+
+    <!-- css style -->
+    <link rel="stylesheet" href="/style/style.css">
+    <!-- font awesome -->
+    <link href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' rel='stylesheet' />
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="#">Spam Seeker</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="mainpage.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about.php">About</a>
+                    <span class="sr-only">(current)</span>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="contact.php">Contact</a>
+                    <span class="sr-only">(current)</span>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"><i class="fa fa-angle-double-up"></i></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<br><br>
+
+<div class="contact">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 mx-auto">
+                <div>
+                    <i class="fa fa-code"></i> by trio of <b>Yotjan</b>
+                    <br>
+                    <br>13516058 - Jonathan Tjandra
+                    <br>13516080 - Putra Hardi Ramadhan
+                    <br>13516134 - Shevalda Gracielira
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+
+</html>
--- mainpage.php
+++ mainpage.php
@@ -1,34 +1,83 @@
+<?php session_start(); ?>
+
 <!DOCTYPE html>
 <html>
 <head>
 
-    <title>Tweet Spam Seeker</title>
+    <meta charset="UTF-8">
+
+    <meta http-equiv="X-UA-Compartible" content="IE=Edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Spam Seeker</title>
 
     <!-- css style -->
     <link rel="stylesheet" href="/style/style.css">
     <!-- font awesome -->
     <link href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' rel='stylesheet' />
-<!--    <link href='https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' rel='stylesheet' />-->
-<!--    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">-->
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <!-- google font -->
+    <link href="https://fonts.googleapis.com/css?family=Berkshire+Swash" rel="stylesheet">
 
 </head>
 
 <body>
-    <h1>Tweet Spam Seeker</h1>
 
-    <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-        Spam Keyword(s): <input type="text" name="keyword" placeholder="pisahkan dengan koma(,)">
-        <br><br>
-        Algortima:
-        <br><input type="radio" name="algorithm" value="kmp" checked> KMP
-        <br><input type="radio" name="algorithm" value="bm"> Boyer-Moore
-        <br><input type="radio" name="algorithm" value="regex"> Regex
-        <br><br>
-        Jumlah tweet: <input input="text" name="jumlah_tweet" placeholder="min. 20, max 200">
-        <br><br>
-        <button type="reset">Reset Pencarian</button>
-        <button type="submit">Cari Tweet Spam</button>
-    </form>
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="#">Spam Seeker</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="mainpage.php">Home
+                            <span class="sr-only">(current)</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.php">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.php">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="fa fa-angle-double-up"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <br><br>
+
+    <!-- Form -->
+    <div class="user-input">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-10 col-md-11 mx-auto">
+                    <div class="form">
+                        <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+                            <input type="text" name="keyword" placeholder="keyword spam" id="keyword">
+                            <br><br>
+                            <select name="algorithm">
+                                <option value="kmp">Knuth-Morris-Pratt</option>
+                                <option value="bm">Boyer-Moore</option>
+                                <option value="regex">Regex</option>
+                            </select>
+                            <br><br>
+                            <input input="text" name="jumlah_tweet" placeholder="number of tweets (20-200)">
+                            <br><br>
+                            <button type="reset" class="btn-form" id="btn-reset">Reset</button>
+                            <button type="submit" class="btn-form" id="btn-submit">Cari</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <?php
 
@@ -39,7 +88,6 @@
             $keyword = $_REQUEST['keyword'];
             $algorithm = $_REQUEST['algorithm'];
 
-            session_start();
             $_SESSION['jumlah_tweet'] = $_REQUEST['jumlah_tweet'];
 
             // dictionary hasil input user
@@ -50,7 +98,8 @@
             fwrite($fp, json_encode($data));
             fclose($fp);
 
-            // require "tools/twitter_api.php";
+            // memanggil API Twitter
+//			require "tools/twitter_api.php";
 
             // menjalankan program kmp dan boyce-moore
             exec('python tools/main.py');
@@ -58,15 +107,53 @@
             //Decode JSON
             $result = json_decode(file_get_contents($input_file), true);
 
-            foreach ($result as $tweet) {
-                echo "<p>";
+            $spam_count = 0;
+            foreach ($result['tweets'] as $tweet) {
                 if ($tweet['spam'] == true) {
-                    echo "<i class=\"fa fa-flag\"></i>";
+                    $spam_count += 1;
                 }
-                echo " <b>" . $tweet['name'] . "</b> - @" . $tweet['username'] . "<br>";
-                echo $tweet['text'] . "<br>";
-                echo "</p>";
             }
+
+            echo "<div class=\"output\">
+                <div class=\"container\">
+                <div class=\"row\">
+                <div class=\"col-lg-10 col-md-11 mx-auto\">";
+
+            echo "<span class=\"result-output\"> Result: ";
+            if ($spam_count == 0) {
+                echo "no tweet contains possible spam";
+            } else if ($spam_count == 1) {
+                echo "one tweet is a possible spam";
+            } else {
+                echo "$spam_count tweets are possible spams";
+            }
+            echo "</span><br>";
+
+            echo "<span class=\"runtime\"> ";
+            if ($algorithm == "kmp") {
+                echo "Knuth-Morris-Pratt";
+            } elseif ($algorithm == "bm") {
+                echo "Boyer-Moore";
+            } else {
+                echo "Regex";
+            }
+            echo " algorithm runtime: " . $result['runtime'] . " s</span>";
+
+            foreach ($result['tweets'] as $tweet) {
+                echo "<div ";
+                if ($tweet['spam'] == true) {
+                    echo " class=\"spam-tweet tweet\">";
+                } else {
+                    echo " class=\"not-spam-tweet tweet\">";
+                }
+                echo "<span class='tweet-text'>". $tweet['text'] . "</span><br>";
+                echo "&nbsp &nbsp";
+                echo "<span class='twitter-name'>- " . $tweet['name'] . "</span>";
+                echo "<span class='twitter-username'> &nbsp @" . $tweet['username'] . "</span>";
+                echo "</div>";
+            }
+
+            echo "</div></div></div></div>";
 
         }
 
